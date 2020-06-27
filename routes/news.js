c850c45b602f60a15020782dabf55434a318ec41
Adrian Mostowski <adaxelx@o2.pl>
2020-06-27 12:03:21 +0200

    add working news (filter) api

--- routes/news.js
+++ routes/news.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const News = require("../models/news");
 
-router.get("/", (req, res) => {
-  res.end();
+router.get("/:id?", (req, res) => {
+  const filter = req.params.id;
+
+  console.log(filter);
+
+  const findData = filter
+    ? News.find({ title: new RegExp(filter, "i") }).sort({
+        date: -1,
+      })
+    : News.find({}).sort({
+        date: -1,
+      });
+  findData.exec((err, data) => {
+    console.log(data);
+    if (!err) {
+      res.json({ data: data, status: 200 });
+    }
+  });
 });
 
 module.exports = router;
